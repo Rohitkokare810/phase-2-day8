08d04474369af2fdde812a96e207e3badd0496e6
RohitKokare810 <Rohitkokare810@gmail.com>
2023-12-18 15:25:30 +0530

    initial commit

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Data.SqlClient;
+
+class Program
+{
+    static void Main()
+    {
+        // Replace the connection string with your actual SQL Server connection string
+        string connectionString = "server=DESKTOP-D9AQSGN;database=Day8Ass;trusted_connection=true;";
+        using (SqlConnection connection = new SqlConnection(connectionString))
+        {
+            connection.Open();
+
+            // Insert 10 records into the Products table
+            InsertSampleData(connection);
+
+            // Display top 5 records in descending order of PName
+            DisplayTop5Products(connection);
+        }
+        Console.ReadKey();
+    }
+
+    static void InsertSampleData(SqlConnection connection)
+    {
+        using (SqlCommand command = connection.CreateCommand())
+        {
+            command.CommandText = @"
+                INSERT INTO Products (Pid, PName, PPrice, MofDate, ExpDate)
+                VALUES
+                ('P00001', 'Product1', 10.99, GETDATE(), DATEADD(DAY, 30, GETDATE())),
+                ('P00002', 'Product2', 20.99, GETDATE(), DATEADD(DAY, 60, GETDATE())),
+                -- Add more records as needed
+                ('P00010', 'Product10', 50.99, GETDATE(), DATEADD(DAY, 90, GETDATE()))";
+
+            command.ExecuteNonQuery();
+        }
+    }
+
+    static void DisplayTop5Products(SqlConnection connection)
+    {
+        using (SqlCommand command = connection.CreateCommand())
+        {
+            command.CommandText = @"
+                SELECT TOP 5 Pid, PName, PPrice, MofDate, ExpDate
+                FROM Products
+                ORDER BY PName DESC";
+
+            using (SqlDataReader reader = command.ExecuteReader())
+            {
+                Console.WriteLine("Top 5 Products:");
+                Console.WriteLine("===============================================");
+                Console.WriteLine("{0,-10} {1,-20} {2,-10} {3,-20} {4,-20}", "Pid", "PName", "PPrice", "MofDate", "ExpDate");
+                Console.WriteLine("===============================================");
+
+                while (reader.Read())
+                {
+                    Console.WriteLine("{0,-10} {1,-20} {2,-10:C2} {3,-20:yyyy-MM-dd} {4,-20:yyyy-MM-dd}",
+                        reader["Pid"], reader["PName"], reader["PPrice"], reader["MofDate"], reader["ExpDate"]);
+                }
+            }
+        }
+    }
+}
---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Data;
+
+class Program
+{
+    static void Main()
+    {
+        // Create a DataTable with the specified schema
+        DataTable productsTable = CreateProductsTable();
+
+        // Insert sample data into the DataTable
+        InsertSampleData(productsTable);
+
+        // Display all products
+        DisplayProducts(productsTable);
+
+        // Search for a product by name
+        SearchProduct(productsTable, "Product3");
+
+        // Delete a product by name
+        DeleteProduct(productsTable, "Product2");
+
+        // Display updated products after deletion
+        DisplayProducts(productsTable);
+        Console.ReadKey();
+    }
+
+    static DataTable CreateProductsTable()
+    {
+        DataTable table = new DataTable("Products");
+
+        // Define columns
+        DataColumn pidColumn = new DataColumn("Pid", typeof(string));
+        DataColumn pnameColumn = new DataColumn("PName", typeof(string));
+        DataColumn ppriceColumn = new DataColumn("PPrice", typeof(decimal));
+        DataColumn mofDateColumn = new DataColumn("MofDate", typeof(DateTime));
+        DataColumn expDateColumn = new DataColumn("ExpDate", typeof(DateTime));
+
+        // Set additional properties
+        pidColumn.Unique = true;
+        pidColumn.MaxLength = 7; // P00001
+
+        pnameColumn.MaxLength = 255; // Adjust as needed
+
+        // Add columns to the DataTable
+        table.Columns.Add(pidColumn);
+        table.Columns.Add(pnameColumn);
+        table.Columns.Add(ppriceColumn);
+        table.Columns.Add(mofDateColumn);
+        table.Columns.Add(expDateColumn);
+
+        return table;
+    }
+
+    static void InsertSampleData(DataTable table)
+    {
+        table.Rows.Add("P00001", "Product1", 10.99, DateTime.Now, DateTime.Now.AddDays(30));
+        table.Rows.Add("P00002", "Product2", 20.99, DateTime.Now, DateTime.Now.AddDays(60));
+        table.Rows.Add("P00003", "Product3", 30.99, DateTime.Now, DateTime.Now.AddDays(90));
+        // Add more rows as needed
+    }
+
+    static void DisplayProducts(DataTable table)
+    {
+        Console.WriteLine("Products:");
+        Console.WriteLine("===============================================");
+        Console.WriteLine("{0,-10} {1,-20} {2,-10} {3,-20} {4,-20}", "Pid", "PName", "PPrice", "MofDate", "ExpDate");
+        Console.WriteLine("===============================================");
+
+        foreach (DataRow row in table.Rows)
+        {
+            Console.WriteLine("{0,-10} {1,-20} {2,-10:C2} {3,-20:yyyy-MM-dd} {4,-20:yyyy-MM-dd}",
+                row["Pid"], row["PName"], row["PPrice"], row["MofDate"], row["ExpDate"]);
+        }
+
+        Console.WriteLine();
+    }
+
+    static void SearchProduct(DataTable table, string productName)
+    {
+        DataRow[] foundRows = table.Select($"PName = '{productName}'");
+
+        if (foundRows.Length > 0)
+        {
+            Console.WriteLine($"Product '{productName}' found:");
+            Console.WriteLine("{0,-10} {1,-20} {2,-10} {3,-20} {4,-20}", "Pid", "PName", "PPrice", "MofDate", "ExpDate");
+            Console.WriteLine("===============================================");
+
+            foreach (DataRow row in foundRows)
+            {
+                Console.WriteLine("{0,-10} {1,-20} {2,-10:C2} {3,-20:yyyy-MM-dd} {4,-20:yyyy-MM-dd}",
+                    row["Pid"], row["PName"], row["PPrice"], row["MofDate"], row["ExpDate"]);
+            }
+
+            Console.WriteLine();
+        }
+        else
+        {
+            Console.WriteLine($"Product '{productName}' not found.");
+        }
+    }
+
+    static void DeleteProduct(DataTable table, string productName)
+    {
+        DataRow[] foundRows = table.Select($"PName = '{productName}'");
+
+        foreach (DataRow row in foundRows)
+        {
+            table.Rows.Remove(row);
+            Console.WriteLine($"Product '{productName}' deleted.");
+        }
+    }
+}
